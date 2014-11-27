94857c8bc559de208b105fe737df59df4c0d6afd
SakuraSa <sakura_kyon@hotmail.com>
2014-11-27 22:55:26 +0800

    add core.configs read config from "config/now.conf" copy "config/default.conf" if "config/now.conf" not exists

---
+++ core/configs.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding = utf-8
+
+"""
+core.configs
+"""
+
+__author__ = 'Rnd495'
+__all__ = ['Configs', 'ConfigsError']
+
+import os
+
+
+# check "config/now.conf"
+# if not exists, create by copying "config/default.conf" to "config/now.conf"
+ROOT_PATH = os.path.split(os.path.split(__file__)[0])[0]
+CONFIG_PATH_NOW = os.path.join(ROOT_PATH, "config/now.conf")
+CONFIG_PATH_DEFAULT = os.path.join(ROOT_PATH, "config/default.conf")
+if not os.path.exists(CONFIG_PATH_NOW):
+    # try to copy from default
+    import shutil
+    shutil.copy(CONFIG_PATH_DEFAULT, CONFIG_PATH_NOW)
+    del shutil
+
+
+class ConfigsError(Exception):
+    """
+    ConfigsError
+    """
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+        self.error_line = kwargs.get('line', None)
+
+
+class Configs(object):
+    """
+    Configs
+    """
+    def __init__(self, config_file_name):
+        with open(config_file_name, 'rb') as file_handle:
+            for line in file_handle:
+                line = line.strip()
+                # lines startswith '#' is commit
+                if not line or line.startswith('#'):
+                    continue
+                separator_index = line.find('=')
+                if separator_index < 0:
+                    raise ConfigsError('ConfigsError: config line syntax error with "%s"' % line)
+                name = line[:separator_index].strip()
+                value = line[separator_index + 1:].strip()
+                # param type parse
+                if value.startswith('"') and value.endswith('"') and len(value) >= 2:
+                    value = value[1:len(value) - 1].replace("\\\"", "\"")
+                    self.__dict__[name] = value
+
+    @staticmethod
+    def instance():
+        if not hasattr(Configs, "_instance"):
+            Configs._instance = Configs(CONFIG_PATH_NOW)
+        return Configs._instance
+
+if __name__ == '__main__':
+    print "NOW_CONFIGS: "
+    print "path:", CONFIG_PATH_NOW
+    for name, value in Configs.instance().__dict__.iteritems():
+        print name, "=", value
