1bb0a8bb405424a9908ce81f617e4a538d55094b
SakuraSa <sakura_kyon@hotmail.com>
2014-11-29 21:36:05 +0800

    add UI.Manager add UI.Page add UI.Module

---
+++ UI/Module.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding = utf-8
+
+"""
+UIModule
+"""
+
+__author__ = 'Rnd495'
+
+
---
+++ UI/Page.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding = utf-8
+
+"""
+Page
+"""
+
+__author__ = 'Rnd495'
