0cca515e3f2179c1f8b60aba43dc6523e094041c
SakuraSa <sakura_kyon@hotmail.com>
2014-12-04 22:46:03 +0800

    mod UI.Page  + PageBase

--- UI/Page.py
+++ UI/Page.py
@@ -6,3 +6,39 @@ Page
 """
 
 __author__ = 'Rnd495'
+
+import tornado.web
+
+import core.models
+from core.models import User
+from core.configs import Configs
+
+configs = Configs.instance()
+
+
+class PageBase(tornado.web.RequestHandler):
+    """
+    PageBase
+    """
+    def __init__(self, application, request, **kwargs):
+        tornado.web.RequestHandler.__init__(self, application, request, **kwargs)
+        self._db_session = core.models.get_global_session()
+        self._current_user = None
+
+    def get_current_user(self):
+        user_id = self.get_secure_cookie("user_id", None)
+        if user_id:
+            self._current_user = self._db_session.query(User).filter(User.id == user_id).first()
+        else:
+            self._current_user = None
+        return self._current_user
+
+    def get_login_url(self):
+        return '/login'
+
+    def data_received(self, chunk):
+        """Implement this method to handle streamed request data.
+
+        Requires the `.stream_request_body` decorator.
+        """
+        raise NotImplementedError()
