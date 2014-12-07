7d6b5e529d8c80de2cb53b2c397f8dd010783f5e
SakuraSa <sakura_kyon@hotmail.com>
2014-12-07 20:41:12 +0800

    add login Page

--- UI/Page.py
+++ UI/Page.py
@@ -10,6 +10,7 @@ __author__ = 'Rnd495'
 import tornado.web
 
 import core.models
+from core import verification
 from core.models import User
 from core.configs import Configs
 from UI.Manager import mapping
@@ -23,8 +24,11 @@ class PageBase(tornado.web.RequestHandler):
     """
     def __init__(self, application, request, **kwargs):
         tornado.web.RequestHandler.__init__(self, application, request, **kwargs)
-        self.db = core.models.get_global_session()
         self._current_user = None
+
+    @property
+    def db(self):
+        return core.models.get_global_session()
 
     def get_current_user(self):
         user_id = self.get_secure_cookie("user_id", None)
@@ -52,3 +56,17 @@ class PageLogin(PageBase):
     def get(self):
         return self.render('login.html')
 
+    @verification.check
+    def post(self):
+        username = self.get_body_argument('username')
+        password = self.get_body_argument('password')
+        password = User.password_hash(password)
+        remember = self.get_body_argument('remember-me', None)
+        expire = 30 if remember else 1
+        redirect = self.get_argument('next', '/')
+        user = self.db.query(User).filter(User.name == username, User.pwd == password).first()
+        if not user:
+            self.redirect('/login?next=%s' % redirect)
+        else:
+            self.set_secure_cookie("user_id", str(user.id), expire)
+            self.redirect(redirect)
