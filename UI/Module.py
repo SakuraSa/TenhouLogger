df1f27a705967c17c662e98227a7835cfb1fe2e2
SakuraSa <sakura_kyon@hotmail.com>
2014-12-07 16:57:02 +0800

    mod UI.Module  + UITitle

--- UI/Module.py
+++ UI/Module.py
@@ -7,4 +7,13 @@ UIModule
 
 __author__ = 'Rnd495'
 
+import tornado.web
+from Manager import mapping
+
+
+@mapping(r'title')
+class UITitle(tornado.web.UIModule):
+    def render(self, title, subtitle=''):
+        return self.render_string('ui/title.html', title=title, subtitle=subtitle)
+
 
